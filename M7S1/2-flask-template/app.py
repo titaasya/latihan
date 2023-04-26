ffcc50e74d5b85b0c09f891b11284742f45dc267
titaasya <alisyatita@gmail.com>
2023-04-26 14:04:37 +0700

    menambahkan code di line 2 dan line akhir

--- M7S1/2-flask-template/app.py
+++ M7S1/2-flask-template/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def iris_prediction():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    run_with_ngrok(app)
+    app.run()
